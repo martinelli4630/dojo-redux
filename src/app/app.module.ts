6729b40d7d5f12b30bd8cd8c99deb137d822b996
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-06-05 12:24:35 -0300

    create angular project and configured redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-
+import { CustomSerializer, metaReducers, rootReducer } from './store.config';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import 'rxjs/Rx';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+
+    StoreModule.forRoot(rootReducer, { metaReducers }),
+    EffectsModule.forRoot([]),
+    environment.production ? [] : StoreDevtoolsModule.instrument(),
   ],
   providers: [],
   bootstrap: [AppComponent]
