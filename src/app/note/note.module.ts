b15a2c93e9b47ad2fb6c2373708f92495c7d6734
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-06-16 09:21:44 -0300

    configured note module and update dependencies

--- src/app/note/note.module.ts
+++ src/app/note/note.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { NoteRoutingModule } from './note-routing.module';
 import { NoteComponent } from './note/note.component';
@@ -8,15 +11,21 @@ import {
   MatButtonModule,
   MatIconModule
 } from '@angular/material';
+import * as fromStore from './store';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     NoteRoutingModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+
+    StoreModule.forFeature('note', fromStore.reducers),
+    EffectsModule.forFeature([...fromStore.effects])
   ],
-  declarations: [NoteComponent]
+  declarations: [NoteComponent],
+  providers: [fromStore.NoteService]
 })
 export class NoteModule { }
