9416890d4f9a7675580e1ec3ba203c1110545b09
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-06-16 09:21:14 -0300

    updated note ui and component

--- src/app/note/note/note.component.ts
+++ src/app/note/note/note.component.ts
@@ -1,5 +1,7 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromStore from '../store';
 
 @Component({
   selector: 'app-note',
@@ -9,13 +11,23 @@ import { Component, OnInit } from '@angular/core';
 export class NoteComponent implements OnInit {
 
   public notes$: Observable<any>;
+  public note;
 
-  constructor() { }
+  constructor(private _store: Store<fromStore.NoteState>) {
+    this.notes$ = this._store.select(fromStore.getNotes);
+  }
 
   ngOnInit() {
+    this._store.dispatch(new fromStore.LoadNote());
   }
 
   onSubmit() {
+    const note = { description: this.note };
+    this._store.dispatch(new fromStore.AddNote(note));
+    this.note = '';
+  }
 
+  onDelete(id) {
+    this._store.dispatch(new fromStore.RemoveNote(id));
   }
 }
