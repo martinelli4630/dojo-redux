59bc9fc48d272144fe4de633155cd836c2cd48d6
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-06-17 18:18:04 -0300

    note and user api

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,14 +1,7 @@
-const api = require('../api');
-const express = require('express');
-const router = express.Router();
+const note = require('./note.js');
+const user = require('./user.js');
 
-router.route('/note')
-    .post(api.insert)
-    .get(api.findAll);
-
-router.route('/note/:id')
-    .delete(api.remove)
-    .get(api.findOne)
-    .put(api.update);
-
-module.exports = router;
+module.exports = {
+  note: note,
+  user: user
+};;
